febaf8a15a5ac530cdaf089a15c83cc291dc1b43
JesseHartsuiker <jhartsuiker@roc-dev.com>
2018-11-12 11:34:30 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 12-11-2018
+ * Time: 11:21
+ */
+$aantal = 14 ;
+$deze_jongen.$aantal."jaar oud!";
+$deze_jongen = "deze jongen is ";
+
+echo $deze_jongen.$aantal."jaar oud!";
+//echo $deze_jongen."deze jongen is";//
